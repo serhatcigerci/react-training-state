1106c0cba7c8cdaeb451612a66abf32efa225109
serhatcigerci <yserhatcigerci@gmail.com>
2023-08-10 18:06:28 +0300

    state training start

---
+++ state/src/Course.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Angular from './assets/angular.jpg'
+import Bootstrap from './assets/bootstrap5.png'
+import CSharp from './assets/ccsharp.png'
+import KompleWeb from './assets/kompleweb.jpg'
+
+export const Course = () => {
+  return (
+    <div>Course</div>
+  )
+}
--- state/src/app.jsx
+++ state/src/app.jsx
@@ -1,15 +1,9 @@
 import './app.css'
-import { useState } from 'react'
 
 export function App() {
-  const [value, setValue] = useState(0)
-  const handleClick = () => {
-    setValue(value + 1)
-  }
   return (
     <>
-    <button onClick={handleClick}>Click me</button>
-    <div>click counter: {value}</div>
+    <button>Click me</button>
     </>
   )
 }
