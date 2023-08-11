3f83ad6087062c9bef68e87d602146854bbc5beb
serhatcigerci <yserhatcigerci@gmail.com>
2023-08-11 17:22:17 +0300

    Map in images

--- state/src/Course.jsx
+++ state/src/Course.jsx
@@ -4,13 +4,17 @@ import Bootstrap from './assets/bootstrap5.png'
 import CSharp from './assets/ccsharp.png'
 import KompleWeb from './assets/kompleweb.jpg'
 
-
+const courseMap = {
+  Angular: Angular,
+  Bootstrap: Bootstrap,
+  CSharp: CSharp,
+  KompleWeb: KompleWeb
+}
 
 export const Course = ({ courseName }) => {
-  console.log(courseName)
   return (
     <div>
-      <img src={Angular} alt="" />
+      <img src={courseMap[courseName]} alt="" />
     </div>
   )
 }
